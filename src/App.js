1786a16d8b276fc71def3f78da945247a468be4b
AbelDagnew <abeldagnew5@gmail.com>
2022-05-31 11:52:35 +0300

    mycom

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import FormCode from './FormCode';
+
+class App extends React.Component {
+  submit = (values) => {
+    console.log(values);
+  }
+  render() {
+    return (
+      <div className="container">
+        <h3 className="jumbotron">Redux Form Validation</h3>
+        <FormCode onSubmit={this.submit} />
+      </div>      
+    )
+  }
+}
+
+export default App;
